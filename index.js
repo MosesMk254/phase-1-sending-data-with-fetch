4dd059d9b51c65a1a3938e5e800340cbddd03951
Moses Mutisya <mutisyamoses722@gmail.com>
2024-04-15 11:51:36 +0300

    using fetch

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email,
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      appendIdToDOM(data.id);
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      appendErrorToDOM(error.message);
+      return error;
+    });
+}
+
+function appendIdToDOM(id) {
+  const newIdElement = document.createElement("p");
+  newIdElement.textContent = `New ID: ${id}`;
+  document.body.appendChild(newIdElement);
+}
+
+function appendErrorToDOM(errorMessage) {
+  const errorElement = document.createElement("p");
+  errorElement.textContent = errorMessage;
+  document.body.appendChild(errorElement);
+}
+
+submitData("Steve", "steve@steve.com");
+submitData("Moses", "mutisyamoses722@gmail.com");
